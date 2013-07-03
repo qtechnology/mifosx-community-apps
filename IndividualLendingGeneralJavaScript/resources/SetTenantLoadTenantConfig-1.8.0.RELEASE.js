319d6cd96efbb694bc96753b57a56ac70d3e2470
Marco Vergaray <mvergaray@quiputechnology.com>
2013-07-03 10:39:59 -0500

    dashboard information

--- IndividualLendingGeneralJavaScript/resources/SetTenantLoadTenantConfig-1.8.0.RELEASE.js
+++ IndividualLendingGeneralJavaScript/resources/SetTenantLoadTenantConfig-1.8.0.RELEASE.js
@@ -280,6 +280,11 @@ custom.helperFunctions = {
 
 		return jQuery.stretchyDataTables
 				.getDataTableEndFormIfNecessary(displayMode);
+	},
+	arrayElement: function(array, i, attr){
+		if(attr)
+			return eval('array[i].'+attr);
+		return array[i];
 	}
 };
 
@@ -739,7 +744,7 @@ custom.showRelatedDataTableInfo = function(tabVar, appTableName,
 						}
 					}
 				}
-			}
+			};
 			// end of local function definitions
 
 			var datatableParams = {
@@ -778,6 +783,11 @@ custom.showRelatedDataTableInfo = function(tabVar, appTableName,
 							+ spaceToUnderscore(data[i].registeredTableName)
 							+ "_id_" + appTablePKValue;
 					tmpObj.itemDivLabel = data[i].registeredTableName;
+					if(tmpObj.registeredTableName === "address and telephone"){
+						tmpObj.itemDivLabel = "Dirección y teléfono";
+						tmpObj.templateName = "addresstelephoneTemplate";
+						tmpObj.type = "template";
+					}
 					datatableArray.push(tmpObj);
 					// alert("added general datatable: " +
 					// data[i].registeredTableName)
